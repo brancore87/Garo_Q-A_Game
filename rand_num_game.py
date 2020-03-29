a61067f657ddc2a3f95f3a08b8918b3652c7418c
Brandon N. Sangma <33341675+brancore87@users.noreply.github.com>
2020-03-30 02:49:49 +0530

    Update rand_num_game.py

--- rand_num_game.py
+++ rand_num_game.py
@@ -1,10 +1,4 @@
 
-# Exercises
-
-# 1. Change the range to be from 0 to 1,000,000.
-# 2. Can you still guess the number?
-# 3. Print the number of guesses made.
-# 4. Limit the number of guesses to the minimum required.
 from random import randint
 
 print("Hai Anching dakode kalna abachengna mo")
